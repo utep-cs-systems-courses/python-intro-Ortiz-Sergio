3c8e0df81c3a1f955191b8cad15318f74f8d43bd
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-01 11:38:34 -0600

    creates a hash table which stores the number of occurences of each word

--- wordCount.py
+++ wordCount.py
@@ -1,9 +1,20 @@
 def wordCount(file):
-    #Checking if we can open and view the file
     cur_file = open(file)
+    hash_table ={}
+
+    current_word = ''
     for line in cur_file:
         for word in line:
-            print(word)
+            word = word.lower()
+            if word.isalpha():
+                current_word += word
+            else:
+                if current_word in hash_table:
+                    hash_table[current_word] += 1
+                else:
+                    hash_table[current_word] = 1
+                current_word = ''
+    print(hash_table)
 
 
 file = input("What file would you like to read? ")
